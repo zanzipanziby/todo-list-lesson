0736dcd61a299e2c15e6dcb558fd0d4135f05022
ZanziPanziBY <zanzipanziby@gmail.com>
2022-10-01 18:33:12 +0300

    first lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import './App.css';
+import TodoList from "./TodoList";
 
 function App() {
+    // BLL
+    const todoListTitle = 'What to learn'
+    const todoListTitle2 = 'What to Drink'
+    const tasks = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: 'Bear', isDone: true},
+        {id: 2, title: 'Cheese', isDone: true},
+        {id: 3, title: 'Fish', isDone: false}
+    ]
+
+    // GUI
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title = {todoListTitle} tasks = {tasks}/>
+            <TodoList title = {todoListTitle2} tasks = {tasks2}/>
         </div>
     );
 }
